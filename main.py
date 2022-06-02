68d2e35ae396ba02688f1c120642c2e83a892264
Adam Valenta <99750261+Adam96Valenta@users.noreply.github.com>
2022-06-02 19:09:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from script import ScraperHeureka
-import time
 
-start_time = time.time()
 scraper = ScraperHeureka('https://graficke-karty.heureka.cz/')
 driver = scraper.open_driver()
 scraper.open_more_pages()
 products_list = scraper.get_products_list()
 df = scraper.get_all_products(products_list[0:2])
 scraper.save_df_to_csv(df, 'Table.csv')
-print("--- %s seconds ---" % (time.time() - start_time))
+
 
