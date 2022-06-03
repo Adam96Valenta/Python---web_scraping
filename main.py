058e5b015bc1eb7031b6f46f837c59e8f35f5415
Adam Valenta <99750261+Adam96Valenta@users.noreply.github.com>
2022-06-03 10:18:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,5 @@ scraper = ScraperHeureka('https://graficke-karty.heureka.cz/')
 driver = scraper.open_driver()
 scraper.open_more_pages()
 products_list = scraper.get_products_list()
-df = scraper.get_all_products(products_list[0:2])
+df = scraper.get_all_products(products_list)
 scraper.save_df_to_csv(df, 'Table.csv')
-
-
